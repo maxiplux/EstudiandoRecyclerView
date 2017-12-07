923f332de6a64a4f5abd87e107a7b4b3e4137bd3
JUAN <juan@Imac-Pegaxo.local>
2017-12-07 17:19:40 -0500

    Initial commit

---
+++ app/src/main/java/net/juanfrancisco/blog/estudiandorecyclerview/Person.kt
@@ -0,0 +1,7 @@
+package net.juanfrancisco.blog.estudiandorecyclerview
+
+/**
+ * Created by juan on 7/12/17.
+ */
+
+class Person internal constructor(var name: String, var age: String, var photoId: Int)
